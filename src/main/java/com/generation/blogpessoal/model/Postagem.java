45c56c11453d0236d8358dc2916168531b921eaa
pedro-hsb <pedro.bernardo4561@gmail.com>
2023-12-08 11:38:11 -0300

    projeto blog pessoal listar todas as postagem

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,61 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="postagens")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length=100)
+	@NotBlank(message="O Atributo Titulo e Obrigatorio")
+	@Size(min=10,max=100,message="o titulo deve ser maior que 10")
+	private String titulo;
+	
+	@Column(length=100)
+	@NotBlank(message="O Atributo texto e Obrigatorio")
+	@Size(min=10,max=1000,message="o titulo deve ser maior que 10")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+}
