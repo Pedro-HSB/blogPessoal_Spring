c74a69165001acaa6480d3427efcec1766b841f2
pedro-hsb <pedro.bernardo4561@gmail.com>
2023-12-14 11:34:12 -0300

    adicao de relacao entr tabelas

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -34,6 +37,11 @@ public class Postagem {
 	@UpdateTimestamp
 	private LocalDateTime data;
 	
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Tema tema;
+    
+	
 	public Long getId() {
 		return id;
 	}
@@ -58,4 +66,12 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+	
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
 }
--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,10 +1,17 @@
 package com.generation.blogpessoal.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Tema {
 		@NotBlank(message="O Atributo descricao e Obrigatorio")
 		@Size(min=5,max=100,message="o descricao deve ser maior que 5")
 		private String descricao;
+		
+	    @OneToMany(fetch = FetchType.LAZY, mappedBy = "tema" , cascade = CascadeType.REMOVE)
+	    @JsonIgnoreProperties("tema")
+	    private List<Postagem> postagem;
 
 		public Long getId() {
 			return id;
@@ -39,4 +50,13 @@ public class Tema {
 		public void setDescricao(String descricao) {
 			this.descricao = descricao;
 		}
+		
+
+	    public List<Postagem> getPostagem() {
+	        return postagem;
+	    }
+
+	    public void setPostagem(List<Postagem> postagem) {
+	        this.postagem = postagem;
+	    }
 }
